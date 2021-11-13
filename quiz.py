fc314462164815da4bab140ae55b4d8d181330bc
Jung <annyeong.@gmail.com>
2021-11-14 00:26:35 +0100

    2nd example

--- quiz.py
+++ quiz.py
@@ -1,3 +1,5 @@
+'''
+# 1.
 # Open a kr.csv and print it's contents.
 
 files = open("kr.csv", "r")
@@ -8,3 +10,18 @@ while True:
     print(file)
 
 files.close()
+
+'''
+
+# 2
+# You can get 10% discount when you spend more than 10000 won
+
+saleprice = int(input("What is your purchaing price? "))
+if saleprice >= 10000:
+    discount = round(saleprice * 0.1,2)
+    print("Purchaing price : {0} won".format(saleprice))
+    print("Discount : {0} won".format(discount))
+    print("Payment amount : {0} won".format(saleprice-discount))
+else:
+    print("You do not get any discount!")
+
